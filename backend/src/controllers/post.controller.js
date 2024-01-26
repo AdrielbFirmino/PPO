e66909d10973e6d2a5b807397b20ee5d05bbb55e
Adriel <adrieldebfirmino@gmail.com>
2024-01-26 16:51:47 -0300

    Backend post pagination

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -22,11 +22,50 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
     try {
-        const post = await postService.findAll();
+        let { limit, offset } = req.query;
+
+        limit = Number(limit);
+        offset = Number(offset);
+
+        if (!limit) {
+            limit = 5;
+        }
+        if (!offset) {
+            offset = 0;
+        }
+
+        const post = await postService.findAll(offset, limit);
+        const total = await postService.countPost();
+        const currentUrl = req.baseUrl
+
+        const next = offset + limit;
+        const nextUrl =
+            next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+        const previous = offset - limit < 0 ? null : offset - limit;
+        const previousUrl =
+            previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
         if (post.length === 0) {
             return res.status(400).send({ message: "There are no posts available" })
         }
-        res.send(post)
+        res.send({
+            nextUrl,
+            previousUrl,
+            limit,
+            offset,
+            total,
+            results: post.map(postItem => ({
+                id: postItem._id,
+                title: postItem.title,
+                body: postItem.body,
+                likes: postItem.likes,
+                comments: postItem.comments,
+                name: postItem.user.name,
+                userName: postItem.user.username,
+                userAvatar: postItem.user.avatar
+            }))
+        })
     } catch (err) {
         res.status(500).send({ message: err.message })
     }
--- backend/src/services/post.service.js
+++ backend/src/services/post.service.js
@@ -2,9 +2,12 @@ import Post from "../models/Post.js";
 
 const create = (body) => Post.create(body);
 
-const findAll = () => Post.find();
+const findAll = (offset, limit) => Post.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
+
+const countPost = () => Post.countDocuments();
 
 export default {
     create,
-    findAll
+    findAll,
+    countPost
 }

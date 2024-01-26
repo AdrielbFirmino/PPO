4c66fbbeb75fcd0f668ca09d3f7065153df3349a
Adriel <adrieldebfirmino@gmail.com>
2024-01-25 23:21:56 -0300

    Auth outset

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,6 @@ const dbPassword = process.env.DB_PASS;
 function connectDatabase() {
   mongoose
     .connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.wwqghrw.mongodb.net/?retryWrites=true&w=majority`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     })
     .then(() => console.log("MongoDB Atlas Connected!"))
     .catch((err) => console.log(`Error connecting to MongoDB Atlas: ${err}`));
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDatabase from './config/db.js';
 import userRoute from './src/routes/user.route.js';
+import authRoute from './src/routes/auth.route.js';
 
 const port = process.env.PORT;
 
@@ -16,8 +17,9 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(cors({credentials: true, origin: "http://localhost:5173"}))
 
-app.use(express.json())
+app.use(express.json());
 app.use("/user", userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`);

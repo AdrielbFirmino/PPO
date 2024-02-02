4e5007cc0943d78bba2ee345653d77a3d5ddab84
Adriel <adrieldebfirmino@gmail.com>
2024-02-01 23:18:22 -0300

    PurpleNote API documentation with swagger

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import connectDatabase from './config/db.js';
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
 import postRoute from './src/routes/post.route.js';
+import swaggerRoute from './src/routes/swagger.route.js'
 
 const port = process.env.PORT;
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/post", postRoute);
+app.use("/doc", swaggerRoute)
 
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`);
